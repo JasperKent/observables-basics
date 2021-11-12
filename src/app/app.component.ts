6297ae7dc84543928e9404ba3ecaacbd2bb6c397
Jasper Kent <software@jasperkent.com>
2021-11-12 08:41:31 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, interval, Observable } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'observables';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('button') button!: ElementRef<HTMLButtonElement>;
+  
+  interval$ = interval (1000).pipe(
+    map (count => count * 6),
+    tap (degrees => console.log('Value is ', degrees))
+  );
+
+  values: (string|number)[] = []
+
+  ngAfterViewInit(): void {
+    fromEvent(this.button.nativeElement, 'click').subscribe(
+      ev => this.values.push('Click')
+    );
+  }
 }
